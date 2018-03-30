ebbc681642933f34da83b21c542b5c5d6003c201
Rahul <rksethuram9@gmail.com>
2018-03-30 15:42:21 -0700

    Use ethcalate

--- src/App.js
+++ src/App.js
@@ -1,122 +1,53 @@
 /* globals state */
-//import { Drizzle, generateStore } from 'drizzle'
-
-import ChannelManagerContract from '../build/contracts/ChannelManager.json'
 
 import React, { Component } from 'react'
 import DesktopComponent from './js/components/DesktopComponent'
 
-import getWeb3 from './utils/getWeb3'
-
-// setup drizzle for react components
-// const options = {
-//   contracts: [
-//     ChannelManagerContract
-//   ]
-// }
-// const drizzleStore = generateStore(this.props.options)
-// const drizzle = new Drizzle(this.props.options, drizzleStore)
+import Ethcalate from './ethcalate-client'
 
 class App extends Component {
   state = {
-    web3: null
+    ethcalate: null,
+    myChannels: []
   }
 
-  async componentWillMount () {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+  async componentDidMount () {
     try {
-      const results = await getWeb3
-      this.setState({
-        web3: results.web3,
-        web3detected: 'true',
-        accountAddress: results.web3.eth.accounts[0]
-      })
-      
-      this.instantiateContract()
-      
+      await this.instantiateContract()
+      await this.getMyChannels()
     } catch (e) {
-      console.log('Error finding web3.')
+      console.log(e)
     }
   }
 
-  instantiateContract () {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-    const contract = require('truffle-contract')
-    const channelManager = contract(ChannelManagerContract)
-    channelManager.setProvider(this.state.web3.currentProvider)
-    channelManager.defaults({ from: this.state.web3.eth.accounts[0]})
+  async instantiateContract () {
+    const ethcalate = new Ethcalate()
+    // TODO: YIKES
+    while (!ethcalate.account) {
+      await ethcalate.initContract()
+    }
     this.setState({
-      channelManager : channelManager
+      ethcalate
     })
+    console.log(ethcalate.channelManager.address)
+  }
 
-    // const simpleStorage = contract(SimpleStorageContract)
-    // simpleStorage.setProvider(this.state.web3.currentProvider)
-
-<<<<<<< HEAD
-    // // Get accounts.
-    // this.state.web3.eth.getAccounts(async (error, accounts) => {
-    //   if (error) {
-    //     console.log(error)
-    //     return
-    //   }
-
-    //   console.log(accounts)
-    //   const channelManagerInstance = await channelManager.deployed()
-    //   this.setState({
-    //     channelManagerInstance : channelManagerInstance,
-    //     channelManagerAddress : channelManagerInstance.address
-    //   })
-    //   console.log(channelManagerInstance.address)
-    // })
-=======
-    // Get accounts.
-    this.state.web3.eth.getAccounts(async (error, accounts) => {
-      if (error) {
-        console.log(error)
-        return
-      }
-
-      console.log(accounts)
-      const channelManagerInstance = await channelManager.deployed()
-      console.log(channelManagerInstance.address)
-
-      // let simpleStorageInstance = await simpleStorage.deployed()
-      // await simpleStorageInstance.set(5, {from: accounts[0]})
-      // let value = await simpleStorageInstance.get.call(accounts[0])
-      // this.setState({ storageValue: value.c[0] })
+  async getMyChannels () {
+    const { ethcalate } = this.state
+    const myChannels = await ethcalate.getMyChannels()
+    console.log('myChannels: ', myChannels)
+    this.setState({
+      myChannels
     })
->>>>>>> 72d8408d5ced13f16e72e9bf1ef4dee512413386
   }
 
   render () {
-    const {
-      web3,
-      web3detected,
-      accountAddress,
-      channelManager
-    } = this.state
-
+    const { ethcalate, myChannels } = this.state
 
     return (
       <div className='App'>
-<<<<<<< HEAD
-                
-=======
-
->>>>>>> 72d8408d5ced13f16e72e9bf1ef4dee512413386
         <div>
-          <DesktopComponent 
-            web3={web3} 
-            web3detected={web3detected}
-            accountAddress={accountAddress}
-            channelManager={channelManager}
-          />
+          <DesktopComponent ethcalate={ethcalate} myChannels={myChannels} />
         </div>
 
       </div>
