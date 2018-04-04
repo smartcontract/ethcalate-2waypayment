f6694bb2303659fa60237da79f1f8e832d6ec40f
Rahul <rksethuram9@gmail.com>
2018-04-04 13:33:53 -0700

    Lint

--- src/js/components/UpdateStateModal.js
+++ src/js/components/UpdateStateModal.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
-import { Button, Modal, Segment, Label, Input, Icon, Container } from 'semantic-ui-react'
+import {
+  Button,
+  Modal,
+  Segment,
+  Label,
+  Input,
+  Icon,
+  Container
+} from 'semantic-ui-react'
 
 class UpdateStateModal extends Component {
-
-    render () {
-        return (
-          <Modal
-            size='small'
-            dimmer='inverted'
-            trigger={
-                <Button>Send Money Shot</Button>
-            }
-            style={{ position: 'absolute', top: '50%', left: '20%' }}
-          >
-            <Modal.Header>Send Your Money Shot</Modal.Header>
-            <Modal.Content>
-              <Label>Here is where stuff goes</Label>
-            </Modal.Content>
-          </Modal>
-        )
-      }
+  render () {
+    return (
+      <Modal
+        size='small'
+        dimmer='inverted'
+        trigger={<Button>Send Money Shot</Button>}
+        style={{ position: 'absolute', top: '50%', left: '20%' }}
+      >
+        <Modal.Header>Send Your Money Shot</Modal.Header>
+        <Modal.Content>
+          <Label>Here is where stuff goes</Label>
+        </Modal.Content>
+      </Modal>
+    )
+  }
 }
 
 export default UpdateStateModal
