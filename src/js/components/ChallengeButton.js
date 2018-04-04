d163db2645dc7b2985825053382d91f39519e6ee
Rahul <rksethuram9@gmail.com>
2018-04-04 13:33:24 -0700

    Join channel

--- src/js/components/ChallengeButton.js
+++ src/js/components/ChallengeButton.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 import { Container, Header, Grid, Accordion, Button } from 'semantic-ui-react'
 
 class ChallengeButton extends Component {
+  handleJoinChannelClick () {
 
-    handleJoinChannelClick () {
-        return
-    }
+  }
 
-    render() {
-        return (
-            <Button onClick={this.handleJoinChannelClick}>Submit Evidence</Button>
-        )
-    }
-
+  render () {
+    return (
+      <Button onClick={this.handleJoinChannelClick}>Submit Evidence</Button>
+    )
+  }
 }
 
 export default ChallengeButton
