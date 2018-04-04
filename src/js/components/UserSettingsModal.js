85353c7dfc5fa4adf32c7aadb4aa3c94df1f68c6
Rahul <rksethuram9@gmail.com>
2018-04-04 13:34:01 -0700

    Lint

--- src/js/components/UserSettingsModal.js
+++ src/js/components/UserSettingsModal.js
@@ -1,45 +1,53 @@
 import React, { Component } from 'react'
-import { Button, Modal, Segment, Label, Input, Icon, Container } from 'semantic-ui-react'
+import {
+  Button,
+  Modal,
+  Segment,
+  Label,
+  Input,
+  Icon,
+  Container
+} from 'semantic-ui-react'
 
 class UserSettingsModal extends Component {
-    state = {
-        phoneNumber: null
+  state = {
+    phoneNumber: null
+  }
+
+  getPhoneNumber () {
+    console.log('getPhoneNumber()')
+    const { ethcalate } = this.props
+
+    if (ethcalate) {
+      console.log(ethcalate.web3.eth.accounts[0])
+      // await ethcalate.updatePhone('4082396181')
     }
 
-    async getPhoneNumber () {
-        console.log('getPhoneNumber()')
-        const { ethcalate } = this.props
+    // const account = ethcalate.web3.eth.accounts[0]
+    // return account
+    return '1234'
+  }
 
-        if (ethcalate) {
-            console.log(ethcalate.web3.eth.accounts[0])
-            // await ethcalate.updatePhone('4082396181')
-
+  render () {
+    return (
+      <Modal
+        size='small'
+        dimmer='inverted'
+        trigger={
+          <Label>
+            <Icon className='cogs' />
+            Settings
+          </Label>
         }
-
-        //const account = ethcalate.web3.eth.accounts[0]
-        //return account
-    }
-
-    render () {
-        return (
-          <Modal
-            size='small'
-            dimmer='inverted'
-            trigger={
-                <Label>
-                    <Icon className='cogs' />
-                    Settings
-                </Label>
-            }
-            style={{ position: 'absolute', top: '50%', left: '20%' }}
-          >
-            <Modal.Header>Settings</Modal.Header>
-            <Modal.Content>
-              <Label>{this.getPhoneNumber()}</Label>
-            </Modal.Content>
-          </Modal>
-        )
-      }
+        style={{ position: 'absolute', top: '50%', left: '20%' }}
+      >
+        <Modal.Header>Settings</Modal.Header>
+        <Modal.Content>
+          <Label>{this.getPhoneNumber()}</Label>
+        </Modal.Content>
+      </Modal>
+    )
+  }
 }
 
 export default UserSettingsModal
