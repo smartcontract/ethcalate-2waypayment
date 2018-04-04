e5b6a9153e4871c5828b2d4ebba8ce3164e41be8
Rahul <rksethuram9@gmail.com>
2018-04-04 13:33:02 -0700

    Ethcalate client should take a web3 object instead of finding its own

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DesktopComponent from './js/components/DesktopComponent'
 
 // import Ethcalate from 'ethcalate'
 import Ethcalate from './ethcalate-client/src/'
+import getWeb3 from './utils/getWeb3'
 
 class App extends Component {
   state = {
@@ -13,15 +14,17 @@ class App extends Component {
 
   async componentDidMount () {
     try {
-      await this.instantiateContract()
+      const results = await getWeb3
+      await this.instantiateContract(results.web3)
       await this.getMyChannels()
     } catch (e) {
       console.log(e)
     }
   }
 
-  async instantiateContract () {
+  async instantiateContract (web3) {
     const ethcalate = new Ethcalate(
+      web3,
       '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf',
       'http://localhost:3000'
     )
