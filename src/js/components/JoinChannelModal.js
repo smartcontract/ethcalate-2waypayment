a79efcb9d4f44fa1d0b0ddd71a816c50e9dee7c7
Rahul <rksethuram9@gmail.com>
2018-04-04 13:33:40 -0700

    Join channel modal

---
+++ src/js/components/JoinChannelModal.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Button, Modal, Label, Form, Input } from 'semantic-ui-react'
+
+class JoinChannelModal extends Component {
+  state = {
+    deposit: 0
+  }
+
+  handleSubmit = () => {
+    const { ethcalate, channelId } = this.props
+    const { deposit } = this.state
+    const depositInWei = ethcalate.web3.toWei(deposit)
+    ethcalate.joinChannel({ channelId, depositInWei })
+  }
+
+  render () {
+    return (
+      <Modal
+        size='small'
+        dimmer='inverted'
+        trigger={<Button disabled={this.props.disabled}>Join Channel</Button>}
+        style={{ position: 'absolute', top: '50%', left: '20%' }}
+      >
+        <Modal.Header>Join Channel</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <Label>Deposit</Label>
+              <Input
+                type='number'
+                placeholder='ETH'
+                name='deposit'
+                onChange={e => this.setState({ deposit: e.target.value })}
+              />
+            </Form.Field>
+            <Button type='submit' onClick={this.handleSubmit}>
+              Open
+            </Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
+
+export default JoinChannelModal
