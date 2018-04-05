8e43874ed76e6f81ea1b5e9cf3a73de7bf9aabf7
Rahul <rksethuram9@gmail.com>
2018-04-04 17:09:26 -0700

    Warnings

--- src/js/components/ChallengeButton.js
+++ src/js/components/ChallengeButton.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import { Container, Header, Grid, Accordion, Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 class ChallengeButton extends Component {
-  handleJoinChannelClick () {
-
-  }
+  handleJoinChannelClick () {}
 
   render () {
     return (
