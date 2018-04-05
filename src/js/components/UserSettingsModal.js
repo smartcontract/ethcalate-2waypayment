7271387c5642cb4198d8a7e99fa8b26ab47d41d8
Rahul <rksethuram9@gmail.com>
2018-04-04 17:55:00 -0700

    Random fixes

--- src/js/components/UserSettingsModal.js
+++ src/js/components/UserSettingsModal.js
@@ -2,50 +2,30 @@ import React, { Component } from 'react'
 import { Modal, Label, Icon } from 'semantic-ui-react'
 
 class UserSettingsModal extends Component {
-<<<<<<< HEAD
-    state = {
-        phoneNumber: null
-    }
-
-    getPhoneNumber () {
-        console.log('getPhoneNumber()')
-        const { ethcalate } = this.props
-=======
   state = {
     phoneNumber: null
   }
 
-  getPhoneNumber () {
-    return '1234'
-  }
->>>>>>> 56106b84ccc11e474899e157ed8fcdbd693885e8
-
-        if (ethcalate) {
-        console.log(ethcalate.web3.eth.accounts[0])
-        // await ethcalate.updatePhone('4082396181')
+  render () {
+    return (
+      <Modal
+        size='small'
+        dimmer='inverted'
+        trigger={
+          <Label>
+            <Icon className='cogs' />
+            Settings
+          </Label>
         }
-    }
-
-    render () {
-        return (
-            <Modal
-            size='small'
-            dimmer='inverted'
-            trigger={
-                <Label>
-                    <Icon className='cogs' />
-                    Settings
-                </Label>
-            }
-            style={{ position: 'absolute', top: '50%', left: '20%' }}
-            >
-            <Modal.Header>Settings</Modal.Header>
-            <Modal.Content>
-                <Label>Insert Phone Number Here</Label>
-            </Modal.Content>
-            </Modal>
-        )
-    }
+        style={{ position: 'absolute', top: '50%', left: '20%' }}
+      >
+        <Modal.Header>Settings</Modal.Header>
+        <Modal.Content>
+          <Label>Insert Phone Number Here</Label>
+        </Modal.Content>
+      </Modal>
+    )
+  }
 }
 
 export default UserSettingsModal
